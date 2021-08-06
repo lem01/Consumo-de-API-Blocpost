f6248c17ba53d33413a211487ab0eb7bb22777b6
lem01 <lemcisneros01@gmail.com>
2021-08-05 18:10:17 -0600

    feature/android-app/lem01/bloc-post-update

--- Blocpost/app/src/main/java/com/example/ejemploretrofit/API/Response.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ejemploretrofit.clases
-
-import com.google.gson.annotations.SerializedName
-
-data class ProductoResponse (
-    @SerializedName("status") var status:String,
-    @SerializedName("datos") var datos: MutableList<Post>)
